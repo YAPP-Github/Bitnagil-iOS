c0f092ffea8f65d9ee154eb71e2f3e1631d9a934
최정인 <wjddls980912@naver.com>
2025-08-06 21:29:33 +0900

    Feat: 온보딩 여부에 따른 진입 뷰 분기 처리
    - 온보딩 여부에 따른 진입 뷰 분기 처리
    - 그에 따른 ViewController, ViewModel 접근제어자 변경 (internal -> public)

--- Projects/Presentation/Sources/Onboarding/View/OnboardingView.swift
+++ Projects/Presentation/Sources/Onboarding/View/OnboardingView.swift
@@ -10,7 +10,7 @@ import Domain
 import SnapKit
 import UIKit
 
-final class OnboardingView: BaseViewController<OnboardingViewModel> {
+public final class OnboardingView: BaseViewController<OnboardingViewModel> {
 
     private enum Layout {
         static let horizontalMargin: CGFloat = 20
@@ -38,7 +38,7 @@ final class OnboardingView: BaseViewController<OnboardingViewModel> {
 
     private let isFromMypage: Bool
     private var cancellables: Set<AnyCancellable>
-    init(
+    public init(
         viewModel: OnboardingViewModel,
         onboarding: OnboardingType,
         isFromMypage: Bool = false
@@ -53,11 +53,11 @@ final class OnboardingView: BaseViewController<OnboardingViewModel> {
         fatalError("init(coder:) has not been implemented")
     }
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
         let stepCount = OnboardingType.allCases.count + 1
@@ -66,7 +66,7 @@ final class OnboardingView: BaseViewController<OnboardingViewModel> {
         self.viewModel.action(input: .fetchOnboardingChoice(onboarding: onboarding))
     }
 
-    override func viewDidLayoutSubviews() {
+    public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
 
         if !isLayoutConfigured {
